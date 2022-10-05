0191a87ff03980d542b17aa09c3321420c992e49
boban anderson <pcboban01@gmail.com>
2022-10-05 11:10:32 +0700

    First Commit

---
+++ app/src/main/java/com/example/penugasan1/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.penugasan1
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.penugasan1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnIntent : Button
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        btnIntent = findViewById(R.id.btn_intent)
+        btnIntent.setOnClickListener(this)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_intent ->{
+                val pindah = Intent(this@MainActivity, MainActivity2::class.java)
+                startActivity(pindah)
+            }
+        }
+    }
+}
